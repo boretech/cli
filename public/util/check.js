2762419e163cacde970fb3b24778be976803b5d8
xiaofuyesnew <xiaofuyesnew@qq.com>
2020-12-08 15:50:15 +0800

    提交更新

--- public/util/check.js
+++ public/util/check.js
@@ -2,7 +2,7 @@
  * @Author: allen.wong 
  * @Date: 2020-11-25 16:18:53 
  * @Last Modified by: allen.wong
- * @Last Modified time: 2020-11-27 17:44:56
+ * @Last Modified time: 2020-12-08 15:44:28
  */
 
 const area = {
@@ -43,8 +43,52 @@ const area = {
   91: "国外"
 }
 
-const idcard = (idStr) => {
-  return false
+const errors = [
+  "验证通过!",
+  "身份证号码位数不对!",
+  "身份证号码出生日期超出范围或含有非法字符!",
+  "身份证号码校验错误!",
+  "身份证地区非法!"
+]
+
+const idcard = idStr => {
+  // 检验输入数据类型
+  if (typeof idStr !== 'string') {
+    console.log(`error: ${errors[2]}`)
+    return false
+  }
+
+  // 检验地区
+  if (area[+idStr.substr(0, 2)] === null) {
+    console.log(`error: ${errors[4]}`)
+    return false
+  }
+
+  // 身份证位数及格式验证
+  if (idStr.length === 18) {
+    const idArr = idStr.split('')
+    const year = +idStr.substr(6, 4)
+    const reg = (!(year % 4) || !(year % 100) && !(year % 400)) ? /^[1-9][0-9]{5}(19|20)[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|[1-2][0-9]))[0-9]{3}[0-9Xx]$/ : /^[1-9][0-9]{5}(19|20)[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|1[0-9]|2[0-8]))[0-9]{3}[0-9Xx]$/
+
+    if (reg.test(idStr)) {
+      // 计算校验位
+      const S = (+idArr[0] + (+idArr[10])) * 7 + (+idArr[1] + (+idArr[11])) * 9 + (+idArr[2] + (+idArr[12])) * 10 + (+idArr[3] + (+idArr[13])) * 5 + (+idArr[4] + (+idArr[14])) * 8 + (+idArr[5] + (+idArr[15])) * 4 + (+idArr[6] + (+idArr[16])) * 2 + (+idArr[7]) * 1 + (+idArr[8]) * 6 + (+idArr[9]) * 3
+      const JYM = '10x98765432'
+      const M = JYM.substr(S % 11, 1)
+      if (M === idArr[17].toLowerCase()) {
+        return true
+      } else {
+        console.log(`error: ${errors[3]}`)
+        return false
+      }
+    } else {
+      console.log(`error: ${errors[3]}`)
+      return false
+    }
+  } else {
+    console.log(`error: ${errors[1]}`)
+    return false
+  }
 }
 
 export default (type, value) => {
