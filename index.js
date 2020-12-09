ac268efcbdc307da049dcb3aac675016aa7b1d03
xiaofuyesnew <xiaofuyesnew@qq.com>
2020-12-09 23:27:32 +0800

    chore: add package

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+#!/usr/bin/env node
+
+const pkg = require('./package.json')
+const cmd = require('commander')
+const progress = require('process')
+const chalk = require('chalk')
+
+console.log(pkg.version)
 console.log('test')
