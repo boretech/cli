898bcf0695d6b318f2384de7dd202d9152e70a0d
xiaofuyesnew <xiaofuyesnew@qq.com>
2020-12-10 23:43:32 +0800

    feat: finish precess

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const {
   version
 } = require('./package.json')
 const {
-  program
+  program,
+  alias
 } = require('commander')
 const progress = require('process')
 const chalk = require('chalk')
@@ -21,7 +22,20 @@ const inquirer = require('inquirer')
 //   loading.succeed(chalk.green('成功！'))
 // }, 5000)
 
-
-
-
-program.version(chalk.gray(`bore-cli ${version}`), '-v, --version', 'output current version of bore-cli').parse(process.argv)
+program
+  .name("bore")
+  .usage("bore create|c [name] [option]")
+  .command('create [name]')
+  .alias('c')
+  .description('create a template project')
+  .option('-w, --web', 'create an H5 template project')
+  .option('-m, --mini', 'create a wx-mini-program template project')
+  .option('-vm, --vue-mobile', 'create a mobile Vue template project with vant')
+  .option('-vp, --vue-pc', 'create a PC Vue template project with element')
+  .action(function (url, cmdObj) {
+    console.log(url)
+    console.log(cmdObj)
+  })
+  .helpOption('-h, --help', 'display help for bore-cli')
+  .version(chalk.gray(`bore-cli ${version}`), '-v, --version', 'output current version of bore-cli')
+  .parse(process.argv)
