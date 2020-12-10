2523e1fd49b7943021259cc38629c2edd1d1b34f
xiaofuyesnew <xiaofuyesnew@qq.com>
2020-12-10 18:37:07 +0800

    chore: new test

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 #!/usr/bin/env node
 
-const pkg = require('./package.json')
-const program = require('commander')
+const {
+  version
+} = require('./package.json')
+const {
+  program
+} = require('commander')
 const progress = require('process')
 const chalk = require('chalk')
+const ora = require('ora')
+const inquirer = require('inquirer')
 
-console.log(pkg.version)
-console.log('test')
+
+
+// const loading = ora(chalk.blue('正在处理'))
+
+// loading.start()
+
+// setTimeout(() => {
+//   loading.succeed(chalk.green('成功！'))
+// }, 5000)
+
+
+
+
+program.version(chalk.gray(`bore-cli ${version}`), '-v, --version', 'output current version of bore-cli').parse(process.argv)
